2b6343dd535939ca8a4b673e367035a9512fdee2
developer-mahin <mdmahin1310@gmail.com>
2023-03-05 00:18:52 +0600

    created post api and get api

--- index.js
+++ index.js
@@ -7,6 +7,41 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.dpsxnyh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+    const postCollection = client.db("banao-project").collection("allPost")
+
+    try {
+
+        app.post("/add-post", async (req, res) => {
+            const query = req.body;
+            const result = await postCollection.insertOne(query)
+            res.send(result)
+        })
+
+        app.get("/all-post", async (req, res) => {
+            const query = {}
+            const result = await postCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(error => console.log(error.message))
+
+
 app.get("/", async (req, res) => {
     res.send("<h1>Banao projects server runnning </h1>")
 })
