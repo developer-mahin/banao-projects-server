e52c9c792f9d3312763c2fa2332f0f5f82eed557
developer-mahin <mdmahin1310@gmail.com>
2023-03-07 00:46:31 +0600

    created a api for getting specific items using id params

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.dpsxnyh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -27,10 +27,23 @@ async function run() {
 
         app.get("/all-post", async (req, res) => {
             const query = {}
+            const result = await postCollection.find(query).sort({ _id: -1 }).toArray()
+            res.send(result)
+        })
+
+        app.get("/role-of-post", async (req, res) => {
+            const role = req.query.role
+            const query = { postRole: role }
             const result = await postCollection.find(query).toArray()
             res.send(result)
         })
 
+        app.get("/post-details/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await postCollection.findOne(query)
+            res.send(result)
+        })
 
     }
 
