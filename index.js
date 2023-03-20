71da2b461351f881c087a65dc657fcb425d0ee68
developer-mahin <mdmahin1310@gmail.com>
2023-03-21 00:30:24 +0600

    create api for access specific user profile

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
-function verifyJWT (req, res, next){
+function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
-    if(!authHeader){
-        res.status(401).send({message: "Unauthorized Access"})
+    if (!authHeader) {
+        res.status(401).send({ message: "Unauthorized Access" })
     }
     const token = authHeader.split(" ")[1]
-    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
-        if(err){
-            res.status(403).send({message: "Unauthorized Access"})
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            res.status(403).send({ message: "Unauthorized Access" })
         }
         req.decoded = decoded
         next()
@@ -35,6 +35,7 @@ function verifyJWT (req, res, next){
 async function run() {
 
     const postCollection = client.db("banao-project").collection("allPost")
+    const usersCollection = client.db("banao-project").collection("users")
 
     try {
 
@@ -46,7 +47,7 @@ async function run() {
         })
 
         // api for upload post
-        app.post("/add-post", async (req, res) => {
+        app.post("/add-post", verifyJWT, async (req, res) => {
             const query = req.body;
             const result = await postCollection.insertOne(query)
             res.send(result)
@@ -60,7 +61,7 @@ async function run() {
         })
 
         // api for specific post
-        app.get("/role-of-post", async (req, res) => {
+        app.get("/role-of-post", verifyJWT, async (req, res) => {
             const role = req.query.role
             const query = { postRole: role }
             const result = await postCollection.find(query).sort({ _id: -1 }).toArray()
@@ -68,10 +69,25 @@ async function run() {
         })
 
         // api for post details
-        app.get("/post-details/:id", async (req, res) => {
+        app.get("/post-details/:id", verifyJWT, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await postCollection.findOne(query)
+            res.send(result)
+        })
+
+        // save user in db
+        app.post("/save-user", verifyJWT, async (req, res) => {
+            const userInfo = req.body
+
+            const result = await usersCollection.insertOne(userInfo)
+            res.send(result)
+        })
+
+        app.get("/profile", async (req, res) => {
+            const queryEmail = req.query.email;
+            const filter = { email: queryEmail }
+            const result = await usersCollection.findOne(filter)
             res.send(result)
         })
 
@@ -86,7 +102,7 @@ run().catch(error => console.log(error.message))
 
 
 app.get("/", async (req, res) => {
-    res.send("<h1>Jobs projects server runnning </h1>")
+    res.send("<h1>Jobs projects server running </h1>")
 })
 
 app.listen(port, () => {
